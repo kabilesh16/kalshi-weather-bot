18137536ba0fd614fee4234a9067e35b0ee6acea
AryanAchuthan07 <aryan.achuthan@berkeley.edu>
2025-12-06 00:43:00 -0800

    Meteo
    Added Meteo Weather

--- kalshi-weather-bot.py
+++ kalshi-weather-bot.py
@@ -1,14 +1,40 @@
 import requests
 
-station_id = "KNYC"  # Central Park, NYC
+station_id = "KNYC"  #Central Park, NYC
 url = f"https://api.weather.gov/stations/{station_id}/observations/latest"
 
 response = requests.get(url)
 data = response.json()
 
-temp_c = data['properties']['temperature']['value']  # in Celsius
+temp_c = data['properties']['temperature']['value']
 precip_mm = data['properties']['precipitationLastHour']['value']
 
 print(f"Temperature: {temp_c} °C")
 print(f"Precipitation last hour: {precip_mm} mm")
-print("hello")
+
+def get_current_weather(lat, lon):
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "current_weather": "true"
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    if "current_weather" not in data:
+        return "No weather data found."
+
+    weather = data["current_weather"]
+
+    return {
+        "temperature_c": weather["temperature"]
+    }
+
+#Los Angeles
+lat = 34.05
+lon = -118.24
+
+weather = get_current_weather(lat, lon)
+print(weather)
